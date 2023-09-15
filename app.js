9d51e442eb8e189bc204b72b061f77e959ff76c7
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-15 22:21:22 +0100

    testing again

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const todoSchema = new mongoose.Schema({
   todo: String,
   date: {
     type: String,
-    default: Date.now(),
+    // default: Date.now(),
   },
   isDone: {
     type: Boolean,
