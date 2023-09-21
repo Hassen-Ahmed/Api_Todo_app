469977f6ff5bda726ea634dde9bbf33caae9e524
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-21 15:35:48 +0100

    alter app logic by push all logic in to controler

--- app.js
+++ app.js
@@ -1,54 +1,22 @@
 const cors = require("cors");
 const express = require("express");
-const Todo = require("./model/test.model");
-
-async function getTodo() {
-  const data = await Todo.find({});
-  return data;
-}
-
-async function postTodo(todo, date, isDone) {
-  const todos = await Todo.create({ todo, date, isDone });
-  todos.save();
-  return todos;
-}
-async function deleteTodo(id) {
-  return await Todo.deleteOne({ _id: id });
-}
-async function patchTodoById(id, todo, isDone) {
-  return await Todo.updateOne({ _id: id }, { $set: { todo, isDone } });
-}
+const {
+  getAllTodo,
+  postTodo,
+  deleteTodo,
+  patchTodoById,
+} = require("./controle/todo.controler");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  getTodo().then((data) => {
-    res.status(200).send({ todos: data });
-  });
-});
+// todo
+app.get("/todo", getAllTodo(req, res));
+app.post("/todo", postTodo(req, res));
+app.delete("/todo/:id", deleteTodo(req, res));
+app.patch("/todo/:id", patchTodoById(req, res));
 
-app.post("/", (req, res) => {
-  const { todo, date = Date.now(), isDone } = req.body;
-  postTodo(todo, date, isDone).then((response) => {
-    res.status(201).send({ todos: response });
-  });
-});
-
-app.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  res.sendStatus(204);
-  deleteTodo(id);
-});
-
-app.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const { todo, isDone } = req.body;
-
-  patchTodoById(id, todo, isDone).then((data) => {
-    res.status(200).send({ todos: data });
-  });
-});
-
+// user
+app.get("/user", getAllUserById(req, res));
 module.exports = app;
