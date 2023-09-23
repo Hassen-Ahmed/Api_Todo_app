385e10ae7690a9da389d210c9a5a9a5c2b76474d
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-23 22:11:48 +0100

    create business/controller logics

--- controle/user.controler.js
+++ controle/user.controler.js
@@ -1,1 +1,24 @@
 const User = require("../model/user.model");
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const userData = await User.find({ _id: id });
+  res.status(200).send({ user: userData });
+};
+const getUserByUsername = async (req, res) => {
+  // const { username } = req.params;
+  const { username } = req.query;
+
+  const userData = await User.find({ username: `${username}` });
+  res.status(200).send({ user: userData });
+};
+
+const postUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.create({ username, password });
+  user.save();
+  res.status(201).send({ user: user });
+};
+
+module.exports = { getUserById, getUserByUsername, postUser };
