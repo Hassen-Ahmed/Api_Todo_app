a47c69110ccbf4ef28b4583ba3c36990d0a64836
Hassen-Ahmed <hassenbest23@gmail.com>
2023-09-29 20:26:26 +0100

    testing cook

--- app.js
+++ app.js
@@ -28,19 +28,4 @@ app.patch("/todo/:id", patchTodoById);
 app.get("/user/:id", getUserById);
 app.get("/user", getUserByUsername);
 app.post("/user", postUser);
-
-app.use("*", (req, res) => {
-  res
-    .cookie("hassenCookies", "{teststring : 'hi there'}", {
-      maxAge: 900000,
-      httpOnly: true,
-      domain: "https://sticky-todos.netlify.app/todo",
-    })
-    .setHeader("Access-Control-Allow-Credentials", true)
-    .setHeader(
-      "Access-Control-Allow-Origin",
-      "https://sticky-todos.netlify.app/todo"
-    )
-    .setHeader("Access-Control-Allow-Header", "X-Requested-with,content-type");
-});
 module.exports = app;
